472454992ce0c96bd0b72cf4294964386e13c57d
Yumihiki <nibu10.majik@gmail.com>
2019-05-25 20:34:52 +0900

    classで一部記述

--- upload.php
+++ upload.php
@@ -6,31 +6,78 @@ $ext = pathinfo($_FILES['upfile']['name']);
 
 $perm = ['pdf'];
 
-if ($_FILES['upfile']['error'] !== UPLOAD_ERR_OK) {
-  $msg = [
-    UPLOAD_ERR_INI_SIZE   => 'php.iniのupload_max_filesize制限を超えています。',
-    UPLOAD_ERR_FORM_SIZE  => 'HTMLのMAX_FILE_SIZE制限を超えています',
-    UPLOAD_ERR_PARTIAL    => 'ファイルが一部しかアップロードされていません',
-    UPLOAD_ERR_NO_FILE    => 'ファイルはアップロードされませんでした',
-    UPLOAD_ERR_NO_TMP_DIR => '一時フォルダが存在しません',
-    UPLOAD_ERR_CANT_WRITE => 'ディスクへの書き込みに失敗しました',
-    UPLOAD_ERR_EXTENSION  => '拡張モジュールによってアップロードが中断されました',
-  ];
+class UploadFile {
+  private $uploadFile = '';
 
-  $err_msg = $msg[$_FILES['upfile']['error']];
-} elseif (!in_array(strtolower($ext['extension']), $perm)) {
-  $err_msg = 'PDF以外のファイルはアップロードできません';
-} elseif ($_FILES['upfile']['type'] !== 'application/pdf' ) {
-  $err_msg = '拡張子をPDFに無理やり変換しないでください';
-} else {
-  $src = $_FILES['upfile']['tmp_name'];
-  // 本と違う書き方
-  $dest = mb_convert_encoding($_FILES['upfile']['name'], 'UTF-8', "JIS, eucjp-win, sjis-win");
-
-  if (!move_uploaded_file($src, 'pdf/'.$dest)) {
-    $err_msg = 'アップロード処理に失敗しました';
+  public function __construct($uploadFile) {
+    $this->setCheck($uploadFile);
   }
+
+  public function setCheck($uploadFile) {
+    $this->uploadFile;
+  }
+
+  public function validation() {
+    $msg = '';
+    if($uploadFile !== UPLOAD_ERR_OK) {
+      $msg = [
+        UPLOAD_ERR_INI_SIZE   => 'php.iniのupload_max_filesize制限を超えています。',
+        UPLOAD_ERR_FORM_SIZE  => 'HTMLのMAX_FILE_SIZE制限を超えています',
+        UPLOAD_ERR_PARTIAL    => 'ファイルが一部しかアップロードされていません',
+        UPLOAD_ERR_NO_FILE    => 'ファイルはアップロードされませんでした',
+        UPLOAD_ERR_NO_TMP_DIR => '一時フォルダが存在しません',
+        UPLOAD_ERR_CANT_WRITE => 'ディスクへの書き込みに失敗しました',
+        UPLOAD_ERR_EXTENSION  => '拡張モジュールによってアップロードが中断されました',
+      ];    
+
+      $err_msg = $msg[$uploadFile];
+    } 
+    // elseif (!in_array(strtolower($ext['extension']), $perm)) {
+    //   $err_msg = 'PDF以外のファイルはアップロードできません';
+    // } elseif ($_FILES['upfile']['type'] !== 'application/pdf' ) {
+    //   $err_msg = '拡張子をPDFに無理やり変換しないでください';
+    // } else {
+    //   $src = $_FILES['upfile']['tmp_name'];
+    //   // 本と違う書き方
+    //   $dest = mb_convert_encoding($_FILES['upfile']['name'], 'UTF-8', "JIS, eucjp-win, sjis-win");
+    
+    //   if (!move_uploaded_file($src, 'pdf/'.$dest)) {
+    //     $err_msg = 'アップロード処理に失敗しました';
+    //   }
+    // }
+  }
+
+
 }
+$a = new UploadFile($_FILES['upfile']['error']);
+
+$a->validation();
+
+// if ($_FILES['upfile']['error'] !== UPLOAD_ERR_OK) {
+//   $msg = [
+//     UPLOAD_ERR_INI_SIZE   => 'php.iniのupload_max_filesize制限を超えています。',
+//     UPLOAD_ERR_FORM_SIZE  => 'HTMLのMAX_FILE_SIZE制限を超えています',
+//     UPLOAD_ERR_PARTIAL    => 'ファイルが一部しかアップロードされていません',
+//     UPLOAD_ERR_NO_FILE    => 'ファイルはアップロードされませんでした',
+//     UPLOAD_ERR_NO_TMP_DIR => '一時フォルダが存在しません',
+//     UPLOAD_ERR_CANT_WRITE => 'ディスクへの書き込みに失敗しました',
+//     UPLOAD_ERR_EXTENSION  => '拡張モジュールによってアップロードが中断されました',
+//   ];
+
+//   $err_msg = $msg[$_FILES['upfile']['error']];
+// } elseif (!in_array(strtolower($ext['extension']), $perm)) {
+//   $err_msg = 'PDF以外のファイルはアップロードできません';
+// } elseif ($_FILES['upfile']['type'] !== 'application/pdf' ) {
+//   $err_msg = '拡張子をPDFに無理やり変換しないでください';
+// } else {
+//   $src = $_FILES['upfile']['tmp_name'];
+//   // 本と違う書き方
+//   $dest = mb_convert_encoding($_FILES['upfile']['name'], 'UTF-8', "JIS, eucjp-win, sjis-win");
+
+//   if (!move_uploaded_file($src, 'pdf/'.$dest)) {
+//     $err_msg = 'アップロード処理に失敗しました';
+//   }
+// }
 
 if (isset($err_msg)) {
   die($err_msg);
@@ -48,6 +95,8 @@ try {
   )";
 
   $stmt = $db->prepare($sql);
+
+  $dest = mb_convert_encoding($_FILES['upfile']['name'], 'UTF-8', "JIS, eucjp-win, sjis-win");
 
   $params = array(':name' =>$dest, ':ip' => $ip, ':path' =>  'pdf/' . $dest);
 
